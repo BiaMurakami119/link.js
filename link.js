d3743f332fa8cea4a36e8edbb9e7305e550daab9
BiaMurakami119 <biamurakami119@gmail.com>
2022-04-26 17:41:20 -0300

    Update link.js

--- link.js
+++ link.js
@@ -1,9 +1,24 @@
-shoot() {
-   var newAngle = cannon.angle - 28;
-    newAngle = newAngle *(3.14/180)
-    var velocity = p5.Vector.fromAngle(newAngle);
-    velocity.mult(0.5);
-    Matter.Body.setStatic(this.body, false);
-    Matter.Body.setVelocity(this.body, {
-      x: velocity.x *(180/3.14), y: velocity.y * (180/3.14)})
+class Boat {
+  constructor(x, y, width, height, boatPos) {
+  
+    this.body = Bodies.rectangle(x, y, width, height);
+    this.width = width;
+    this.height = height;
+
+    this.image = loadImage("./assets/boat.png");
+    this.boatPosition = boatPos;
+    World.add(world, this.body);
   }
+z
+  display() {
+    var angle = this.body.angle;
+    var pos = this.body.position;
+
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    imageMode(CENTER);
+    image(this.image, 0, this.boatPosition, this.width, this.height);
+    pop();
+  }
+}
