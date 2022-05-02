d186c55235d2030cd9db60ce4911d152afaa7f16
BiaMurakami119 <biamurakami119@gmail.com>
2022-05-02 15:40:35 -0300

    Update link.js

--- link.js
+++ link.js
@@ -1,30 +1,48 @@
-function showBoats() {
-  if (boats.length > 0) {
-    if (
-      boats[boats.length - 1] === undefined ||
-      boats[boats.length - 1].body.position.x < width - 300
-    ) {
-      var positions = [-40, -60, -70, -20];
-      var position = random(positions);
-      var boat = new Boat(width, height - 100, 170, 170, position);
+//////show life
 
-      boats.push(boat);
-    }
+showLife() {
+    push();
+    image(lifeImage, width / 2 - 130, height - player.positionY - 400, 20, 20);
+    fill("white");
+    rect(width / 2 - 100, height - player.positionY - 400, 185, 20);
+    fill("#f50057");
+    rect(width / 2 - 100, height - player.positionY - 400, player.life, 20);
+    noStroke();
+    pop();
+  }
 
-    for (var i = 0; i < boats.length; i++) {
-      if (boats[i]) {
-        Matter.Body.setVelocity(boats[i].body, {
-          x: -0.9, 
-          y: 0
-        });
 
-        boats[i].display();
-      } else {
-        boats[i];
-      }
-    }
-  } else {
-    var boat = new Boat(width, height - 60, 170, 170, -60);
-    boats.push(boat);
+
+
+
+/////////show rank
+showRank() {
+    swal({
+      title: `Incrível!${"\n"}Rank${"\n"}${player.rank}`,
+      text: "Você alcançou a linha de chegada com sucesso!",
+      imageUrl:
+        "https://raw.githubusercontent.com/vishalgaddam873/p5-multiplayer-car-race-game/master/assets/cup.png",
+      imageSize: "100x100",
+      confirmButtonText: "Ok"
+    });
+  }
+
+
+
+
+
+
+
+
+///////////////gameover
+  gameOver() {
+    swal({
+      title: `Fim de Jogo`,
+      text: "Oops você perdeu a corrida!",
+      imageUrl:
+        "https://cdn.shopify.com/s/files/1/1061/1924/products/Thumbs_Down_Sign_Emoji_Icon_ios10_grande.png",
+      imageSize: "100x100",
+      confirmButtonText: "Obrigado por jogar"
+    });
   }
 }
