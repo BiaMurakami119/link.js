3183fcbe413f82548103077e153e8a3762b102f3
BiaMurakami119 <biamurakami119@gmail.com>
2022-05-05 17:30:43 -0300

    Update link.js

--- link.js
+++ link.js
@@ -1,81 +1,22 @@
-///show fuel
-showFuelBar() {
-    push();
-    image(fuelImage, width / 2 - 130, height - player.positionY - 350, 20, 20);
-    fill("white");
-    rect(width / 2 - 100, height - player.positionY - 350, 185, 20);
-    fill("#ffc400");
-    rect(width / 2 - 100, height - player.positionY - 350, player.fuel, 20);
-    noStroke();
-    pop();
-  }
+class Link{
+    constructor(bodyA,bodyB)
+    {
+      var lastlink = bodyA.body.bodies.length-2;
+     this.link = Constraint.create(
+        {
+          bodyA:bodyA.body.bodies[lastlink],
+          pointA:{x:0,y:0},
+          bodyB:bodyB,
+          pointB:{x:0,y:0},
+          length:-10,
+          stiffness:0.01
+        });
+        World.add(engine.world,this.link);
+    } 
 
-
-
-
-
-//////show life
-
-showLife() {
-    push();
-    image(lifeImage, width / 2 - 130, height - player.positionY - 400, 20, 20);
-    fill("white");
-    rect(width / 2 - 100, height - player.positionY - 400, 185, 20);
-    fill("#f50057");
-    rect(width / 2 - 100, height - player.positionY - 400, player.life, 20);
-    noStroke();
-    pop();
-  }
-
-
-
-
-
-/////////show rank
-showRank() {
-    swal({
-      title: `Incrível!${"\n"}Rank${"\n"}${player.rank}`,
-      text: "Você alcançou a linha de chegada com sucesso!",
-      imageUrl:
-        "https://raw.githubusercontent.com/vishalgaddam873/p5-multiplayer-car-race-game/master/assets/cup.png",
-      imageSize: "100x100",
-      confirmButtonText: "Ok"
-    });
-  }
-
-
-
-
-
-
-
-
-///////////////gameover
-  gameOver() {
-    swal({
-      title: `Fim de Jogo`,
-      text: "Oops você perdeu a corrida!",
-      imageUrl:
-        "https://cdn.shopify.com/s/files/1/1061/1924/products/Thumbs_Down_Sign_Emoji_Icon_ios10_grande.png",
-      imageSize: "100x100",
-      confirmButtonText: "Obrigado por jogar"
-    });
-  }
+    dettach()
+    {
+      World.remove(engine.world,this.link);
+     
+    }
 }
-
-
-
-
-
-
-//////////SWAL colocar no index.html
-<!-- Sweet Alert-->
-    <script
-      src="https://code.jquery.com/jquery-3.5.1.min.js"
-      integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
-      crossorigin="anonymous"
-    ></script>
-    <script src="./lib/sweetalert.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="./lib/sweetalert.css" />
-
-    <link rel="stylesheet" href="style.css" />
